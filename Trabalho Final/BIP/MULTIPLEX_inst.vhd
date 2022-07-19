dadcb74e1415627f754d9a8b6de5fcda5f0566f0
kakimage <dkmoaz@gmail.com>
2022-07-19 12:33:04 -0300

    começando trabalho final

---
+++ Trabalho Final/BIP/MULTIPLEX_inst.vhd
@@ -0,0 +1,7 @@
+MULTIPLEX_inst : MULTIPLEX PORT MAP (
+		clock	 => clock_sig,
+		data_in	 => data_in_sig,
+		sload_data	 => sload_data_sig,
+		result	 => result_sig,
+		result_valid	 => result_valid_sig
+	);
