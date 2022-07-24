dec302ff25496c589ad89d37b249e90e82b85a02
leandro-kakimoto <leandro.kakimoto@radixeng.com.br>
2022-07-24 15:13:57 -0300

    adding mux

--- Trabalho Final/BIP/MULTIPLEX_inst.vhd
+++
@@ -1,7 +0,0 @@
-MULTIPLEX_inst : MULTIPLEX PORT MAP (
-		clock	 => clock_sig,
-		data_in	 => data_in_sig,
-		sload_data	 => sload_data_sig,
-		result	 => result_sig,
-		result_valid	 => result_valid_sig
-	);
---
+++ Trabalho Final/BIP/MUX3X1.vhd
@@ -0,0 +1,26 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity MUX3X1 is 
+port (A: in std_logic_vector(15 downto 0);
+		B: in std_logic_vector(15 downto 0);
+		C: in std_logic_vector(15 downto 0);
+		S: in std_logic_vector(1 downto 0);
+		Q: out std_logic_vector(15 downto 0)
+
+);
+end MUX3X1;
+
+architecture Behavioral of MUX3X1 is
+BEGIN
+    PROCESS(S)
+    BEGIN
+		 	case S is
+				when "00" => Q <= A;
+				when "01" => Q <= B;
+				when "10" => C <= C;
+				when others => report "unreachable" severity failure;
+	end case;
+	END PROCESS;
+end architecture;
